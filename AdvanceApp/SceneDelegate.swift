64b0d900e6031dca7cb8ad4bf6cf5a1d7275ab5d
가현 <102145605+rkgus24@users.noreply.github.com>
2025-07-25 17:46:50 +0900

    feat: 프로젝트 기본 세팅

---
+++ AdvanceApp/SceneDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  SceneDelegate.swift
+//  AdvanceApp
+//
+//  Created by 노가현 on 7/25/25.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = UINavigationController(rootViewController: ViewController())
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+    
+    func sceneDidDisconnect(_ scene: UIScene) {}
+    
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+    
+    func sceneWillResignActive(_ scene: UIScene) {}
+    
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+    
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+}
---
+++ AdvanceApp/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  AdvanceApp
+//
+//  Created by 노가현 on 7/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("viewDidLoad")
+    }
+}
